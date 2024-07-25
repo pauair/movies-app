b4af17e2f7bb8f90ffdad92394cead9f0f066e91
Paula Airaudo <paula.airaudo@hotmail.com>
2024-07-25 18:04:12 -0300

    chore: update import statements for MovieProvider, and Movies components

--- components/MovieProvider.jsx
+++ components/MovieProvider.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Image, View } from 'react-native';
+import React from "react";
+import { Image, View } from "react-native";
 
 const MovieProvider = ({ movie }) => {
     return (
-        <View className='flex-row'>
+        <View className='flex-row flex-wrap '>
             {movie.providers.US.flatrate &&
                 movie.providers.US.flatrate.map((provider, index) => (
                     <Image
-                    alt={provider.provider_name}
-                    key={index}
-                    source={{
-                        uri: `https://image.tmdb.org/t/p/w500${provider.logo_path}`,
-                    }}
-                    style={{ width: 50, height: 50 }}
-                    className='rounded-lg m-2 p-2'
-                />
+                        alt={provider.provider_name}
+                        key={index}
+                        source={{
+                            uri: `https://image.tmdb.org/t/p/w500${provider.logo_path}`,
+                        }}
+                        style={{ width: 50, height: 50 }}
+                        className='rounded-lg m-2 p-2'
+                    />
                 ))}
         </View>
     );
--- components/Movies.jsx
+++ components/Movies.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Button, FlatList, Text, TextInput, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { fetchAllMovies, fetchWatchProviders, searchMovieByName } from '../lib/api-movies';
-import Loading from './Loading';
-import { AnimatedMovieCard } from './MovieCard';
-
+import React, { useEffect, useState } from "react";
+import { Button, FlatList, Text, TextInput, View } from "react-native";
+import {
+    fetchAllMovies,
+    fetchWatchProviders,
+    searchMovieByName,
+} from "../lib/api-movies";
+import Loading from "./Loading";
+import { AnimatedMovieCard } from "./MovieCard";
 
 const Movies = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [query, setQuery] = useState('');
-    const insets = useSafeAreaInsets();
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         const getAllMovies = async () => {
@@ -45,25 +46,26 @@ const Movies = () => {
     }
 
     return (
-        <View
-            className='pl-2 pr-2 items-center'
-            style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
-        >
-            <Text className='text-5xl pt-8 font-bold'>MOVIES</Text>
+        <View className='pl-2 pr-2 items-center bg-black'>
             <View className='flex-row items-center justify-center p-4'>
-              <TextInput
-                placeholder='Search movie...'
-                value={query}
-                onChangeText={setQuery}
-                className='py-2 mx-2 w-2/3 rounded-xl border-b-2 border-blue-300 text-center'
-              />
-            <Button className='rounded-xl py-2 mx-2 w-1/3' title='Search' onPress={handleSearch} disabled={query===''}/>
+                <TextInput
+                    placeholder='Search movie...'
+                    value={query}
+                    onChangeText={setQuery}
+                    className='py-2 mx-2 w-2/3 rounded-xl border-b-2 border-blue-300 text-center text-white '
+                />
+                <Button
+                    className='rounded-xl py-2 mx-2 w-1/3'
+                    title='Search'
+                    onPress={handleSearch}
+                    disabled={query === ""}
+                />
             </View>
             <FlatList
                 data={movies}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item, index }) => (
-                  <AnimatedMovieCard movie={item} index={index} />
+                    <AnimatedMovieCard movie={item} index={index} />
                 )}
             />
         </View>
