4d0fcbf6792b67b2b271299e5ebfd75a65425613
Paula Airaudo <paula.airaudo@hotmail.com>
2024-07-25 18:07:04 -0300

    feat: add Layout component for better code organization and readability

---
+++ app/_layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+    return (
+        <View className='bg-black h-full'>
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#000',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                        fontSize: 48,
+                    },
+                    headerTitle: 'Movies',
+                    headerTitleAlign: 'center',
+
+                }}
+            />
+        </View>
+    );
+}
